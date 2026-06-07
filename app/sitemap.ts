c6c1fde9c9baf3873929b1f73597631e9db58c72
Dimasrizky <dimasrizky822@gmail.com>
2026-06-07 11:43:03 +0700

    add

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://pakaisiku.id'
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/support`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+  ]
+}
